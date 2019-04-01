7c29bffd906324ecb4684bd6d533e8c044a3bdc4
Gilad Lev Ari <GiladL@cet.ac.il>
2019-04-01 16:37:46 +0300

    Set initial state to undefined when falsy

--- src/useStoreCreator.js
+++ src/useStoreCreator.js
@@ -7,8 +7,10 @@ export const useStoreCreator = (state, reducer) => {
     reducer
   });
 
+  state = state ? { ...state } : undefined;
+
   const ACTION_TYPE = '@@INIT_STATE';
-  const [store, dispatch] = useReducer(reducer, reducer({ ...state }, { ACTION_TYPE }));
+  const [store, dispatch] = useReducer(reducer, reducer(state, { ACTION_TYPE }));
   const [action, setAction] = useState(ACTION_TYPE);
 
   store.__dispatch = dispatch;
